215434be099f9171ce4929a727bbbcf989b54920
Malay Pandya <112810176+Malay94@users.noreply.github.com>
2022-09-06 16:28:51 +0530

    Add files via upload

---
+++ week 01part02.js
@@ -0,0 +1,85 @@
+// document.write("This is Day:02 (Operators and functions)");
+// document.write("<br><br>");
+// //ans //or //not operators in JS
+
+// //&& and operator
+// age=23;
+// y= 'doesDrive';
+
+// if(age>=18 && y=='doesDrive'){
+//     document.write("Yes! You can drive your car");
+// }
+// else
+// {
+//     document.write("You can't Drive");
+// }
+
+// //or operator
+
+// document.write("<br><br>");
+// m="creaditCard";
+// p="cash";
+
+// if(m=="creaditCard" || p=="phonepay"){
+//     document.write("You chose digital method for pay money");
+// }
+// else
+// {
+//     document.write("OK! You pay cash");
+// }
+
+// //not operator
+// document.write("<br><br>");
+
+// p="no-cash";
+
+// if(!(p='cash')){
+//     document.write("You chose digital method for pay money");
+// }
+// else
+// {
+//     document.write("OK! You pay cash");
+// }
+
+// *******(Function)********
+
+//There are four type of function 
+//  1.NO argument ,no return
+//  2. argument , return
+//  3.NO argument , return
+//  4.argument ,no return
+
+//Type 01
+
+// function show(){
+//     n1=13;
+//     n2=23;
+//     document.write("Here! The := ",n1,'<br>',n2,'<br>');
+// }
+// show();
+
+//Type 02
+
+// function show(n1,n2){
+//     document.write("Your no. is",n1+n2);
+//     return n1,n2;
+// }
+// show(10,20);
+
+//Type 03
+
+// function show(){
+
+//     n1=13; n2=23;
+//     document.write("Here! The := ",n1,'<br>',n2,'<br>',n1+n2);
+//     return n1,n2;
+// }
+// show();
+
+//Type 04
+
+// function show(n1,n2){
+//     document.write("Your no. is",n1+n2);
+// }
+// show(20,30);
+
